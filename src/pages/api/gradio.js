8146e2a30bd93e8ad9b8d7adf7f3738353907d77
victorcodebase <victorkithinji@outlook.com>
2025-02-03 22:01:37 +0300

    input log

--- src/pages/api/gradio.js
+++ src/pages/api/gradio.js
@@ -6,6 +6,7 @@ export default async function handler(req, res) {
 		return res.status(405).json({ error: "Method Not Allowed" });
 	}
 	try {
+		console.log("Data fed to Gradio: ", req.body);
 		const client = await Client.connect("mark-kithinji/meadowai-crop-predictor");
 		const result = await client.predict("/predict", req.body);
 		res.status(200).json(result);
