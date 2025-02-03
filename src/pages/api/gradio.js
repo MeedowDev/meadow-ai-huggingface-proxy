7c7d9b70062a557196a1a11ca59accf24cb21bfb
victorcodebase <victorkithinji@outlook.com>
2025-02-03 21:25:08 +0300

    endpoint err 500 fix

--- src/pages/api/gradio.js
+++ src/pages/api/gradio.js
@@ -1,19 +1,17 @@
-// pages/api/gradio.js
-import { createClient } from "@gradio/client";
-
-// Replace with your actual Gradio app URL
-const gradioClient = createClient("https://huggingface.co/spaces/mark-kithinji/meadowai-crop-predictor");
+import { Client } from "@gradio/client";
 
 export default async function handler(req, res) {
+	console.log("Request received: ", req.method);
 	if (req.method !== "POST") {
 		return res.status(405).json({ error: "Method Not Allowed" });
 	}
 	try {
-		const { data } = req.body; // Expecting { data: [...] } from your app
-        console.log("Data received: ", data);
-		const prediction = await gradioClient.predict(data);
-		res.status(200).json(prediction);
+		const client = await Client.connect("mark-kithinji/meadowai-crop-predictor");
+		const result = await client.predict("/predict", req.body);
+		res.status(200).json(result);
+		
 	} catch (error) {
+		console.log("Error: ", error);
 		res.status(500).json({ error: error.message });
 	}
 }
