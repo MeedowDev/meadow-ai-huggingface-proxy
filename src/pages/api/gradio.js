fdb274820e018f5bb208235e1183bb815525fe2a
victorcodebase <victorkithinji@outlook.com>
2025-03-31 11:23:35 +0300

    payload format error patch

--- src/pages/api/gradio.js
+++ src/pages/api/gradio.js
@@ -1,5 +1,4 @@
 import { Client } from "@gradio/client";
-// Mark says hie
 export default async function handler(req, res) {
 	if (req.method !== "POST") {
 		return res.status(405).json({ error: "Method Not Allowed" });
@@ -7,7 +6,7 @@ export default async function handler(req, res) {
 	try {
 		console.log("Data fed to Gradio: ", req.body);
 		const client = await Client.connect("mark-kithinji/meadowai-crop-predictor");
-		const result = await client.predict("/predict", req.body);
+		const result = await client.predict("/predict", { input_json_str: JSON.stringify(req.body) });
 		res.status(200).json(result);
 		
 	} catch (error) {
